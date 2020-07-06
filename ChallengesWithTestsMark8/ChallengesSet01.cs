cf8e95af75d74ed0649576503a5a2037a957d6a5
CHJohnston <cdh.johnston@gmail.com>
2020-07-06 17:25:47 -0500

    Challenge Set 1 Completed

--- ChallengesWithTestsMark8/ChallengesSet01.cs
+++ ChallengesWithTestsMark8/ChallengesSet01.cs
@@ -1,42 +1,71 @@
 ﻿using System;
+using System.Text;
 
 namespace ChallengesWithTestsMark8
 {
     public class ChallengesSet01
+    //Complete -- All Tests Passed!
     {
         public bool AreTwoNumbersTheSame(int num1, int num2)
         {
-            throw new NotImplementedException();            
+            //Complete - This Method returns a bool (true or false) if two integers are the same
+            if (num1 == num2)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
 
         public double Subtract(double minuend, double subtrahend)
         {
-            throw new NotImplementedException();
+            //Complete - This method Subtracts one number from another
+            return minuend - subtrahend;
         }
 
         public int Add(int number1, int number2)
         {
-            throw new NotImplementedException();
+            //Complete - This method adds two intergers
+            return number1 + number2;
         }
 
         public int GetSmallestNumber(int number1, int number2)
-        {
-            throw new NotImplementedException();
+        {   //Complete - This method determines the smaller of two integers
+            if (number1 < number2)
+            {
+                return number1;
+            }
+            else 
+            {
+                return number2;
+            }
         }
 
         public long Multiply(long factor1, long factor2)
         {
-            throw new NotImplementedException();
+            //Complete - This Method multiplies two numbers and returns the product
+            return factor1 * factor2;            
         }
 
         public string GetGreeting(string nameOfPerson)
         {
-            throw new NotImplementedException();
+            //Complete - This Method Returns "Hello" with the name of the person
+          
+            if (nameOfPerson == "")
+            {
+                return "Hello!";
+            }
+            else 
+            {
+                return "Hello, " + nameOfPerson + "!";
+            }        
         }
 
         public string GetHey()
         {
-            throw new NotImplementedException();
+            return "HEY!";
         }
     }
 }
